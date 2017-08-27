0516e2977a48888f0ea45479fba33af58154e557
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-08-27 23:21:09 +0100

    Added bootstrap and basic layout in html

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let app = require('express')();
-let http = require('http').Server(app);
-let port = process.env.PORT || 3000;
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
 
 app.use('/css', express.static(path.join(__dirname, '/public/assets/css')));
+app.use('/js', express.static(path.join(__dirname, '/public/assets/js')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/views/index.html');
